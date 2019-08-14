9e90feae267d63dab46cca3695d0365a5c76ba3f
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2019-08-14 10:29:55 -0500

    af16b3e

---
+++ _next/static/webpack/static/development/pages/_app.js.21431a09ab0bfaeeb597.hot-update.js
@@ -0,0 +1,48 @@
+webpackHotUpdate("static/development/pages/_app.js",{
+
+/***/ "./utils/analytics/gtm.ts":
+/*!********************************!*\
+  !*** ./utils/analytics/gtm.ts ***!
+  \********************************/
+/*! exports provided: GTM_ID, GTM_URL, pageView, event */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(process) {/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "GTM_ID", function() { return GTM_ID; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "GTM_URL", function() { return GTM_URL; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "pageView", function() { return pageView; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "event", function() { return event; });
+var GTM_CONFIG = {
+  ID: process.env.GTM_ID || '',
+  AUTH: process.env.GTM_AUTH || '',
+  PREVIEW: process.env.GTM_PREVIEW || '',
+  COOKIES_WIN: process.env.GTM_COOKIES_WIN || ''
+};
+var GTM_ID = GTM_CONFIG.ID;
+var GTM_URL = ["https://www.googletagmanager.com/gtag/js?id=".concat(GTM_CONFIG.ID), GTM_CONFIG.AUTH && "gtm_auth=".concat(GTM_CONFIG.AUTH), GTM_CONFIG.PREVIEW && "gtm_preview=".concat(GTM_CONFIG.PREVIEW), GTM_CONFIG.COOKIES_WIN && "gtm_cookies_win=".concat(GTM_CONFIG.COOKIES_WIN)].filter(Boolean).join('&');
+var pageView = function pageView(url) {
+  if (typeof window.gtag === 'function') {
+    window.gtag('config', GTM_CONFIG.ID, {
+      page_path: url
+    });
+  }
+};
+var event = function event(_ref) {
+  var action = _ref.action,
+      category = _ref.category,
+      label = _ref.label;
+
+  if (typeof window.gtag === 'function') {
+    window.gtag('event', action, {
+      event_category: category,
+      event_label: label
+    });
+  }
+};
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../../../../node_modules/process/browser.js */ "../../node_modules/process/browser.js")))
+
+/***/ })
+
+})
+//# sourceMappingURL=_app.js.21431a09ab0bfaeeb597.hot-update.js.map
